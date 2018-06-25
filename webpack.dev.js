643ed018b4360c183af30aea5c59a9decb91ad27
Arief Novianto <arief.novianto8@gmail.com>
2018-06-25 16:13:39 +0700

    fixing auto rebuild problem

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,10 +1,16 @@
 const merge = require('webpack-merge');
+const path = require('path');
 const common = require('./webpack.common.js');
 const ReloadServerPlugin = require('reload-server-webpack-plugin'); // add an instant server for development mode and automatically reload feature
 
 module.exports = merge.multiple(common, {
 	server: {
 		mode: 'development',
+		watch: true,
+		watchOptions: {
+			ignored: /node_modules/,
+			poll: true // resolve not automatic rebuild
+		},
 		plugins: [
 			new ReloadServerPlugin({
 				script: './.build/app.js',
