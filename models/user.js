2eb67d9a7d83411b8f6e85421a3e6c3b710f3c82
Arief Novianto <arief.novianto8@gmail.com>
2018-06-26 17:24:38 +0700

    add user model

---
+++ models/user.js
@@ -0,0 +1,85 @@
+import mongoose, { Schema } from 'mongoose';
+import validator from 'validator';
+import uniqueValidator from 'mongoose-unique-validator';
+import mongooseId from 'mongoose-id';
+import debugLogger from 'debug-logger';
+
+import { removePassChar } from '../libs/remove-punctuation';
+
+const log = debugLogger('userModel');
+
+const userSchema = new Schema(
+	{
+		username: {
+			type: String,
+			required: [true, 'Can not be empty!'],
+			unique: true,
+			uniqueCaseInsensitive: true,
+			validate: [
+				{
+					validator: (v) => {
+						const isValid = validator.isAlphanumeric(v);
+
+						log.info(`return username validation result, ${isValid}`);
+						return isValid;
+					},
+					message: 'Must only use letters and numbers!',
+				},
+				{
+					validator: (v) => {
+						const isValid = validator.isLength(v, { min: 5, max: 15 });
+
+						log.info(`return username validation length result, ${isValid}`);
+						return isValid;
+					},
+					message: 'Must be between 5 and 15 characters!',
+				},
+			],
+		},
+		email: {
+			type: String,
+			required: [true, 'Can not be empty!'],
+			unique: true,
+			uniqueCaseInsensitive: true,
+			validate: [
+				{
+					validator: (v) => {
+						const isValid = validator.isEmail(v);
+
+						log.info(`return email validation result, ${isValid}`);
+						return isValid;
+					},
+					message: 'Email is invalid!',
+				},
+			],
+		},
+		password: {
+			type: String,
+			required: [true, 'Can not be empty!'],
+			validate: [
+				{
+					validator: (v) => {
+						let item = v;
+						item = removePassChar(item);
+						const isValid = validator.isAlphanumeric(item);
+
+						log.info(`return password validation result, ${isValid}`);
+						return isValid;
+					},
+					message: 'Must only use letters, numbers and password special characters by owasp except space and backslash!',
+				},
+			],
+		},
+	},
+	{
+		timestamps: false,
+		toJSON: { virtuals: false },
+	},
+);
+
+userSchema.plugin(mongooseId);
+userSchema.plugin(uniqueValidator);
+
+const UserModel = mongoose.model('User', userSchema);
+
+export default UserModel;
