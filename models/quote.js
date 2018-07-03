4a1d4ee7d357cb28e601c97a02ebaf78217d13bc
Arief Novianto <arief.novianto8@gmail.com>
2018-07-04 06:27:15 +0700

    use rules and encrypt password on pre save

--- models/quote.js
+++ models/quote.js
@@ -7,66 +7,65 @@ import removePunctuation from '../libs/remove-punctuation';
 
 const log = debugLogger('quoteModel');
 
-const quoteSchema = new Schema(
-	{
-		content: {
-			type: String,
-			required: [true, 'Can not be empty!'],
-			validate: [
-				{
-					validator: (v) => {
-						let item = v;
-						item = removePunctuation(item);
-						const isValid = validator.isAlphanumeric(item);
+export const quoteRules = {
+	content: {
+		type: String,
+		required: [true, 'Can not be empty!'],
+		validate: [
+			{
+				validator: (v) => {
+					let item = v;
+					item = removePunctuation(item);
+					const isValid = validator.isAlphanumeric(item);
 
-						log.info(`return content validation result, ${isValid}`);
-						return isValid;
-					},
-					message: 'Must only use letters, numbers, spaces, and punctuation!',
+					log.info(`return content validation result, ${isValid}`);
+					return isValid;
 				},
-				{
-					validator: (v) => {
-						const isValid = validator.isLength(v, { min: 10, max: 100 });
+				message: 'Must only use letters, numbers, spaces, or punctuation!',
+			},
+			{
+				validator: (v) => {
+					const isValid = validator.isLength(v, { min: 10, max: 100 });
 
-						log.info(`return content validation length result, ${isValid}`);
-						return isValid;
-					},
-					message: 'Must be between 10 and 100 characters!',
+					log.info(`return content validation length result, ${isValid}`);
+					return isValid;
 				},
-			],
-		},
-		inventor: {
-			type: String,
-			required: [true, 'Can not be empty!'],
-			validate: [
-				{
-					validator: (v) => {
-						let item = v;
-						item = removePunctuation(item);
-						const isValid = validator.isAlphanumeric(item);
-
-						log.info(`return inventor validation result, ${isValid}`);
-						return isValid;
-					},
-					message: 'Must only use letters, numbers, spaces, and punctuation!',
-				},
-				{
-					validator: (v) => {
-						const isValid = validator.isLength(v, { min: 5, max: 30 });
-
-						log.info(`return content validation length result, ${isValid}`);
-						return isValid;
-					},
-					message: 'Must be between 5 and 20 characters!',
-				},
-			],
-		},
+				message: 'Must be between 10 and 100 characters!',
+			},
+		],
 	},
-	{
-		timestamps: false,
-		toJSON: { virtuals: false },
+	inventor: {
+		type: String,
+		required: [true, 'Can not be empty!'],
+		validate: [
+			{
+				validator: (v) => {
+					let item = v;
+					item = removePunctuation(item);
+					const isValid = validator.isAlphanumeric(item);
+
+					log.info(`return inventor validation result, ${isValid}`);
+					return isValid;
+				},
+				message: 'Must only use letters, numbers, spaces, or punctuation!',
+			},
+			{
+				validator: (v) => {
+					const isValid = validator.isLength(v, { min: 5, max: 30 });
+
+					log.info(`return content validation length result, ${isValid}`);
+					return isValid;
+				},
+				message: 'Must be between 5 and 20 characters!',
+			},
+		],
 	},
-);
+};
+
+const quoteSchema = new Schema(quoteRules, {
+	timestamps: false,
+	toJSON: { virtuals: false },
+});
 
 quoteSchema.plugin(mongooseId);
 
--- models/user.js
+++ models/user.js
@@ -3,92 +3,99 @@ import validator from 'validator';
 import uniqueValidator from 'mongoose-unique-validator';
 import mongooseId from 'mongoose-id';
 import debugLogger from 'debug-logger';
+import bcrypt from 'bcrypt';
+
+import bcryptConfig from '../configs/bcrypt';
 
 import { removePassChar } from '../libs/remove-punctuation';
 
 const log = debugLogger('userModel');
 
-const userSchema = new Schema(
-	{
-		username: {
-			type: String,
-			required: [true, 'Can not be empty!'],
-			unique: true,
-			uniqueCaseInsensitive: true,
-			validate: [
-				{
-					validator: (v) => {
-						const isValid = validator.isAlphanumeric(v);
+export const userRules = {
+	username: {
+		type: String,
+		required: [true, 'Can not be empty!'],
+		unique: true,
+		uniqueCaseInsensitive: true,
+		validate: [
+			{
+				validator: (v) => {
+					const isValid = validator.isAlphanumeric(v);
 
-						log.info(`return username validation result, ${isValid}`);
-						return isValid;
-					},
-					message: 'Must only use letters and numbers!',
+					log.info(`return username validation result, ${isValid}`);
+					return isValid;
 				},
-				{
-					validator: (v) => {
-						const isValid = validator.isLength(v, { min: 5, max: 15 });
+				message: 'Must only use letters or numbers!',
+			},
+			{
+				validator: (v) => {
+					const isValid = validator.isLength(v, { min: 5, max: 15 });
 
-						log.info(`return username validation length result, ${isValid}`);
-						return isValid;
-					},
-					message: 'Must be between 5 and 15 characters!',
+					log.info(`return username validation length result, ${isValid}`);
+					return isValid;
 				},
-			],
-		},
-		email: {
-			type: String,
-			required: [true, 'Can not be empty!'],
-			unique: true,
-			uniqueCaseInsensitive: true,
-			validate: [
-				{
-					validator: (v) => {
-						const isValid = validator.isEmail(v);
-
-						log.info(`return email validation result, ${isValid}`);
-						return isValid;
-					},
-					message: 'Email is invalid!',
-				},
-			],
-		},
-		password: {
-			type: String,
-			required: [true, 'Can not be empty!'],
-			validate: [
-				{
-					validator: (v) => {
-						let item = v;
-						item = removePassChar(item);
-						const isValid = validator.isAlphanumeric(item);
-
-						log.info(`return password validation result, ${isValid}`);
-						return isValid;
-					},
-					message: 'Must only use letters, numbers and password special characters by owasp except space and backslash!',
-				},
-				{
-					validator: (v) => {
-						const isValid = validator.isLength(v, { min: 8, max: 20 });
-
-						log.info(`return username validation length result, ${isValid}`);
-						return isValid;
-					},
-					message: 'Must be between 8 and 20 characters!',
-				},
-			],
-		},
+				message: 'Must be between 5 and 15 characters!',
+			},
+		],
 	},
-	{
-		timestamps: false,
-		toJSON: { virtuals: false },
+	email: {
+		type: String,
+		required: [true, 'Can not be empty!'],
+		unique: true,
+		uniqueCaseInsensitive: true,
+		validate: [
+			{
+				validator: (v) => {
+					const isValid = validator.isEmail(v);
+
+					log.info(`return email validation result, ${isValid}`);
+					return isValid;
+				},
+				message: 'Email is invalid!',
+			},
+		],
 	},
-);
+	password: {
+		type: String,
+		required: [true, 'Can not be empty!'],
+		validate: [
+			{
+				validator: (v) => {
+					let item = v;
+					item = removePassChar(item);
+					const isValid = validator.isAlphanumeric(item);
+
+					log.info(`return password validation result, ${isValid}`);
+					return isValid;
+				},
+				message: 'Must only use letters, numbers, or password special characters by owasp except space or backslash!',
+			},
+			{
+				validator: (v) => {
+					const isValid = validator.isLength(v, { min: 8, max: 20 });
+
+					log.info(`return username validation length result, ${isValid}`);
+					return isValid;
+				},
+				message: 'Must be between 8 and 20 characters!',
+			},
+		],
+	},
+};
+
+const userSchema = new Schema(userRules, {
+	timestamps: false,
+	toJSON: { virtuals: false },
+});
 
 userSchema.plugin(mongooseId);
 userSchema.plugin(uniqueValidator, {
 	message: 'Already used by others!',
+});
+
+userSchema.pre('save', function encryptPassword(next) {
+	this.password = bcrypt.hashSync(this.password, bcryptConfig.saltRounds);
+	next();
 });
 
 const UserModel = mongoose.model('User', userSchema);
