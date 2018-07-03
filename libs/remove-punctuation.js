27981578f345eff37a385147a5c68393d700fb62
Arief Novianto <arief.novianto8@gmail.com>
2018-07-04 06:23:21 +0700

    update remove-punctuation and add validate-with-mongoose libs

--- libs/remove-punctuation.js
+++ libs/remove-punctuation.js
@@ -1,24 +1,29 @@
 import validator from 'validator';
 import debugLogger from 'debug-logger';
+import isEmpty from 'is-empty';
 
 const log = debugLogger('removePunctuation');
 
 const removePunctuation = (word) => {
+	if (isEmpty(word)) return word;
+
 	const punctuation = [' ', '.', ',', ';', ':', '?', '!', '.', "'", '"', '(', ')', '/', '[', '-']; // dash character must be the last element
 	const closedBracket = ']'; // closed bracket not supported by validator.js blacklist
 
-	log.info('return word without puctuation');
+	log.info('return word without punctuation');
 	return validator.blacklist(word.replace(closedBracket, ''), punctuation);
 };
 
 export const removePassChar = (pass) => { // not allowed to use space and backslash
+	if (isEmpty(pass)) return pass;
+
 	const punctuation = [
 		'!', '"', '#', '$', '%', '&', "'", '(', ')', '*', '+', ',', '.', '/', ':',
 		';', '<', '=', '>', '?', '@', '[', '^', '_', '`', '{', '|', '}', '~', '-',
 	]; // dash character must be the last element
 	const closedBracket = ']'; // closed bracket not supported by validator.js blacklist
 
-	log.info('return pass without puctuation');
+	log.info('return pass without punctuation');
 	return validator.blacklist(pass.replace(closedBracket, ''), punctuation);
 };
 
