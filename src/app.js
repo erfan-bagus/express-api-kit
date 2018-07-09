d2caddef381acc243f86f0063372edbc4cce631d
Arief Novianto <arief.novianto8@gmail.com>
2018-07-09 08:34:33 +0700

    add favicon, some information, semantic ui, and jquery

--- src/app.js
+++ src/app.js
@@ -14,13 +14,52 @@ database(); // configure database
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(express.static('src/static'));
 
 cors(app); // configure cors
 
 api(app); // add api
 
 app.get('/', (req, res) => {
-	res.status(404).send('Sorry, I am under constructions!');
+	res.send(`
+		<!DOCTYPE html>
+		<html>
+			<head>
+				<!-- Hey, Dasar KEPO! -->
+				<title>Express API Kit</title>
+				<link rel="icon" href="/favicons/16.ico" sizes="16x16" />
+
+				<link rel="stylesheet" type="text/css" href="/others/semantic-ui/semantic.min.css" />
+			</head>
+			<body>
+				<div style="padding: 10px;">
+					<div class="ui relaxed divided list">
+						<div class="item">
+							<div class="content">
+								<a class="header" href="/api/quote">/api/quote</a>
+								<div class="description" style="font-size: 11px;">( GET | POST | PUT | DELETE )</div>
+							</div>
+						</div>
+						<div class="item">
+							<div class="content">
+								<a class="header">sensitive information</a>
+								<div class="description" style="font-size: 11px;">...</div>
+							</div>
+						</div>
+						<div class="item">
+							<div class="content">
+								<a class="header">sensitive information</a>
+								<div class="description" style="font-size: 11px;">...</div>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<script src="/others/jquery/jquery-3.3.1.min.js"></script>
+				<script src="/others/semantic-ui/semantic.min.js"></script>
+			</body>
+		</html>
+	`);
 });
 
 server(app); // start server
